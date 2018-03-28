dd926f84b4548b53103735863361f7050049f5ad
Patrick Connolly <patrick.c.connolly@gmail.com>
2018-03-28 11:49:08 -0400

    Allowed gsheet ID to be set via envvar.

--- web/data.php
+++ web/data.php
@@ -3,7 +3,8 @@
 
 Rapi Castillo
 */
-  $url = "https://docs.google.com/spreadsheets/d/1pREvLn2nidERGfXPQ2Q49D-VCMUcETx6A9JVusv5xu8/export?gid=0&format=csv";
+  $gsheet_id = getenv('TOOLKIT_GSHEET_ID');
+  $url = "https://docs.google.com/spreadsheets/d/{$gsheet_id}/export?gid=0&format=csv";
   $content = file_get_contents($url);
 //  $data = json_decode($content);
 
